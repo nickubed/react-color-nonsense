48f2491c4c8e6e5dc4401f51dd525069f4bd5e4c
nickubed <nhpquandt@gmail.com>
2020-05-09 09:54:25 -0700

    More pruning

--- src/ColorForm.jsx
+++ src/ColorForm.jsx
@@ -2,25 +2,20 @@ import React, {useState} from 'react'
 
 const ColorForm = (props) => {
     let [myColor, setMyColor] = useState('')
-    
-    const addColor = props.addColor
-    
+
     const setColor = (input) => {
         setMyColor(input)
         myColor = input
     }
-
-    console.log('Color is: ', myColor)
-
     const handleSubmit = (e) => {
         e.preventDefault()
-        addColor(myColor)
+        props.addColor(myColor)
     }
 
     return (
         <div>
             <form>
-                <input type="text" name="userColor" value={myColor} onChange={(e) =>{setColor(e.target.value)}}></input>
+                <input type="text" name="userColor" value={myColor} onChange={(e) => {setColor(e.target.value)}}></input>
                 <button type="submit" onClick={(e) => {handleSubmit(e)}}>Send it</button>
             </form>
         </div>
